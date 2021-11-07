2364a393986eed25ea51cd2b819d674eafa3f73e
msengbusch <marek.sengbusch@flg-online.de>
2021-11-07 10:46:00 +0100

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    java
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Lwjgl Wrapper (Graphics, Audio, Input, ...)
+    implementation("com.badlogicgames.gdx:gdx-backend-lwjgl:1.10.0")
+}
