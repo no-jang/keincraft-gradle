9e5b2f5d412abf9b638684133e58d0e66734ad14
msengbusch <marek.sengbusch@flg-online.de>
2021-11-07 14:56:05 +0100

    add vulkan, assimp, openal, bgfx and imgui as dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,24 +12,54 @@ sourceSets {
     }
 }
 
-val lwjglNatives = arrayOf("natives-linux", "natives-linux-arm32", "natives-linux-arm64", "natives-macos", "natives-windows", "natives-windows-x86")
+val nativeOS = arrayOf("natives-linux", "natives-macos", "natives-windows")
+val nativeLibraries = arrayOf(
+    "org.lwjgl.lwjgl",
+    "org.lwjgl.lwjgl-glfw", // Window, Input
+    "org.lwjgl.lwjgl-opengl", // Graphics API
+    "org.lwjgl.lwjgl-stb", // Image Loading
+    "org.lwjgl.lwjgl-vulkan", // Graphics API
+    "org.lwjgl.lwjgl-assimp", // Model Loading
+    "org.lwjgl.lwjgl-jemalloc", // Memory Allocator
+    "org.lwjgl.lwjgl-openal", // Audio
+    "org.lwjgl.lwjgl-bgfx", // Graphics Wrapper
+    "org.lwjgl.lwjgl-vma" // Vulkan Memory Allocator
+)
 
 dependencies {
-    // LWJGL
-    implementation(platform("org.lwjgl:lwjgl-bom:3.2.3"))
+    implementation(platform("org.lwjgl:lwjgl-bom:3.2.3")) // Lwjgl Library Versions
 
-    implementation("org.lwjgl", "lwjgl-stb") // Textures
+    implementation("org.lwjgl", "lwjgl") // Core
+    implementation("org.lwjgl", "lwjgl-opengl") // Graphics API
+    implementation("org.lwjgl", "lwjgl-glfw") // Window, Input
+    implementation("org.lwjgl", "lwjgl-stb") // Image Loading
+    implementation("org.lwjgl", "lwjgl-vulkan") // Graphics API
+    implementation("org.lwjgl", "lwjgl-assimp") // Model Loading
+    implementation("org.lwjgl", "lwjgl-jemalloc") // Memory Allocator
+    implementation("org.lwjgl", "lwjgl-openal") // Audio
+    implementation("org.lwjgl", "lwjgl-bgfx") // Graphics Wrapper
+    implementation("org.lwjgl", "lwjgl-vma") // Vulkan Memory Allocator
 
-    // Native libraries
-    lwjglNatives.forEach { native ->
+    implementation("io.github.spair", "imgui-java-lwjgl3", "1.84.1.3") // GUI
+
+    implementation("org.joml:joml:1.10.2")
+
+    nativeOS.forEach { native ->
+        runtimeOnly("org.lwjgl", "lwjgl", classifier = native)
+        runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = native)
+        runtimeOnly("org.lwjgl", "lwjgl-glfw", classifier = native)
         runtimeOnly("org.lwjgl", "lwjgl-stb", classifier = native)
+        runtimeOnly("org.lwjgl", "lwjgl-assimp", classifier = native)
+        runtimeOnly("org.lwjgl", "lwjgl-jemalloc", classifier = native)
+        runtimeOnly("org.lwjgl", "lwjgl-openal", classifier = native)
+        runtimeOnly("org.lwjgl", "lwjgl-bgfx", classifier = native)
+        runtimeOnly("org.lwjgl", "lwjgl-vma", classifier = native)
+
+        runtimeOnly("io.github.spair", "imgui-java-$native", "1.84.1.3")
     }
 
-    // Lwjgl Wrapper (Graphics, Audio, Input, ...)
-    implementation("com.badlogicgames.gdx:gdx-backend-lwjgl3:1.10.0")
-
-    // Math
-    implementation("org.joml:joml:1.10.2")
+    // Only needed for macos
+    runtimeOnly("org.lwjgl", "lwjgl-vulkan", classifier = "natives-macos")
 }
 
 tasks.create<Sync>("copyLibraries") {
