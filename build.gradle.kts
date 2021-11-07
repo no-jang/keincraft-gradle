72290d1b253dac0e25159053ea6e8655c614085d
msengbusch <marek.sengbusch@flg-online.de>
2021-11-07 10:52:39 +0100

    add minecraft-clone as source set

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,15 @@ repositories {
     mavenCentral()
 }
 
+sourceSets {
+    main {
+        java {
+            srcDir("minecraft-clone")
+        }
+    }
+}
+
 dependencies {
     // Lwjgl Wrapper (Graphics, Audio, Input, ...)
-    implementation("com.badlogicgames.gdx:gdx-backend-lwjgl:1.10.0")
+    implementation("com.badlogicgames.gdx:gdx-backend-lwjgl3:1.10.0")
 }
