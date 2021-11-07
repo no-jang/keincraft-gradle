1ed98b3453aab053b6dc3626fd2cecb9570a7cd8
msengbusch <marek.sengbusch@flg-online.de>
2021-11-07 11:02:23 +0100

    make it work again

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,13 +8,26 @@ repositories {
 
 sourceSets {
     main {
-        java {
-            srcDir("minecraft-clone")
-        }
+        java.srcDir("minecraft-clone")
     }
 }
 
+val lwjglNatives = arrayOf("natives-linux", "natives-linux-arm32", "natives-linux-arm64", "natives-macos", "natives-windows", "natives-windows-x86")
+
 dependencies {
+    // LWJGL
+    implementation(platform("org.lwjgl:lwjgl-bom:3.2.3"))
+
+    implementation("org.lwjgl", "lwjgl-stb") // Textures
+
+    // Native libraries
+    lwjglNatives.forEach { native ->
+        runtimeOnly("org.lwjgl", "lwjgl-stb", classifier = native)
+    }
+
     // Lwjgl Wrapper (Graphics, Audio, Input, ...)
     implementation("com.badlogicgames.gdx:gdx-backend-lwjgl3:1.10.0")
+
+    // Math
+    implementation("org.joml:joml:1.10.2")
 }
