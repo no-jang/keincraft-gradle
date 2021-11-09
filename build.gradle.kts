07585277d67eec27f7b810967fd39f2ecaed7abc
msengbusch <marek.sengbusch@flg-online.de>
2021-11-09 22:50:40 +0100

    First vulkan steps

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,14 +30,14 @@ dependencies {
     implementation(platform("org.lwjgl:lwjgl-bom:3.2.3")) // Lwjgl Library Versions
 
     implementation("org.lwjgl", "lwjgl") // Core
-    implementation("org.lwjgl", "lwjgl-opengl") // Graphics API
+    //implementation("org.lwjgl", "lwjgl-opengl") // Graphics API
     implementation("org.lwjgl", "lwjgl-glfw") // Window, Input
     implementation("org.lwjgl", "lwjgl-stb") // Image Loading
     implementation("org.lwjgl", "lwjgl-vulkan") // Graphics API
     implementation("org.lwjgl", "lwjgl-assimp") // Model Loading
-    implementation("org.lwjgl", "lwjgl-jemalloc") // Memory Allocator
+    //implementation("org.lwjgl", "lwjgl-jemalloc") // Memory Allocator
     implementation("org.lwjgl", "lwjgl-openal") // Audio
-    implementation("org.lwjgl", "lwjgl-bgfx") // Graphics Wrapper
+    //implementation("org.lwjgl", "lwjgl-bgfx") // Graphics Wrapper
     implementation("org.lwjgl", "lwjgl-vma") // Vulkan Memory Allocator
 
     implementation("io.github.spair", "imgui-java-lwjgl3", "1.84.1.3") // GUI
@@ -46,13 +46,13 @@ dependencies {
 
     nativeOS.forEach { native ->
         runtimeOnly("org.lwjgl", "lwjgl", classifier = native)
-        runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = native)
+        //runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = native)
         runtimeOnly("org.lwjgl", "lwjgl-glfw", classifier = native)
         runtimeOnly("org.lwjgl", "lwjgl-stb", classifier = native)
         runtimeOnly("org.lwjgl", "lwjgl-assimp", classifier = native)
-        runtimeOnly("org.lwjgl", "lwjgl-jemalloc", classifier = native)
+        //runtimeOnly("org.lwjgl", "lwjgl-jemalloc", classifier = native)
         runtimeOnly("org.lwjgl", "lwjgl-openal", classifier = native)
-        runtimeOnly("org.lwjgl", "lwjgl-bgfx", classifier = native)
+        //runtimeOnly("org.lwjgl", "lwjgl-bgfx", classifier = native)
         runtimeOnly("org.lwjgl", "lwjgl-vma", classifier = native)
 
         runtimeOnly("io.github.spair", "imgui-java-$native", "1.84.1.3")
