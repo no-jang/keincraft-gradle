39b9225fc9d5ff7300b8c2cf15d56045c8971fca
msengbusch <marek.sengbusch@flg-online.de>
2021-11-20 11:50:41 +0100

    revamp devices

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,15 @@ repositories {
 
 sourceSets {
     main {
-        java.srcDir("keincraft")
+        java {
+            srcDir("keincraft")
+        }
+
+        resources {
+            srcDir("keincraft")
+
+            include("tinylog.properties")
+        }
     }
 }
 
@@ -31,19 +39,19 @@ dependencies {
     implementation(platform("org.lwjgl:lwjgl-bom:3.3.0-SNAPSHOT")) // Lwjgl Library Versions
 
     implementation("org.lwjgl", "lwjgl") // Core
-    //implementation("org.lwjgl", "lwjgl-opengl") // Graphics API
     implementation("org.lwjgl", "lwjgl-glfw") // Window, Input
     implementation("org.lwjgl", "lwjgl-stb") // Image Loading
     implementation("org.lwjgl", "lwjgl-vulkan") // Graphics API
     implementation("org.lwjgl", "lwjgl-assimp") // Model Loading
-    //implementation("org.lwjgl", "lwjgl-jemalloc") // Memory Allocator
     implementation("org.lwjgl", "lwjgl-openal") // Audio
-    //implementation("org.lwjgl", "lwjgl-bgfx") // Graphics Wrapper
     implementation("org.lwjgl", "lwjgl-vma") // Vulkan Memory Allocator
 
     implementation("io.github.spair", "imgui-java-lwjgl3", "1.84.1.3") // GUI
 
-    implementation("org.joml:joml:1.10.2")
+    // Util
+    implementation("org.joml:joml:1.10.2") // Math
+    implementation("org.tinylog:tinylog-api:2.4.0-M2") // Logging
+    implementation("org.tinylog:tinylog-impl:2.4.0-M2") // Logging
 
     nativeOS.forEach { native ->
         runtimeOnly("org.lwjgl", "lwjgl", classifier = native)
