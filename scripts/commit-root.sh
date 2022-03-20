645e04ab913fcb07edf4c1b14806d187f618ac59
msengbusch <66240817+msengbusch@users.noreply.github.com>
2022-03-20 15:48:36 +0100

    hello

--- scripts/commit-root.sh
+++ scripts/commit-root.sh
@@ -11,12 +11,12 @@ mkdir -p $TMP_DIR
 
 (
   cd engine/common/src/main/java || exit
-  git diff --relative HEAD^ HEAD . > ../../../../../$TMP_DIR/reset.patch
+  git diff --relative HEAD^ HEAD . > ../../../../../$TMP_DIR/commit-root.patch
 )
 
 (
   cd aio || exit
-  git apply --allow-empty --reject ../$TMP_DIR/reset.patch
+  git apply --allow-empty --reject ../$TMP_DIR/commit-root.patch
   git add .
   git commit -m "[SYNC]"
 )
